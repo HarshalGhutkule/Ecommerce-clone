b3edfd940e688c32100be0fa878a2df8831793d3
Harshal Ghutkule <95927895+HarshalGhutkule@users.noreply.github.com>
2021-12-17 00:53:45 +0530

    Landing page
    90% page done

--- landingPage.js
+++ landingPage.js
@@ -4,21 +4,23 @@
 document.querySelector(".next").addEventListener("click", nextimage);
 document.querySelector(".prev").addEventListener("click", previmage);
 
-var image = [ "https://cdn.shopify.com/s/files/1/0449/5225/6667/files/Picture5_1512x.jpg?v=1637134351",
-"https://cdn.shopify.com/s/files/1/0449/5225/6667/files/Papaya-desktop-banner_1512x.jpg?v=1633939948",
-"https://cdn.shopify.com/s/files/1/0449/5225/6667/files/1_1512x.jpg?v=1599587622",
-"https://cdn.shopify.com/s/files/1/0449/5225/6667/files/Banner_14_09_1512x.jpg?v=1600179622"
+var image = [ "https://cdn.shopify.com/s/files/1/0449/5225/6667/files/1_1512x.jpg?v=1599587622",
+"https://cdn.shopify.com/s/files/1/0449/5225/6667/files/Banner_14_09_1512x.jpg?v=1600179622",
+"https://cdn.shopify.com/s/files/1/0449/5225/6667/files/Picture5_1512x.jpg?v=1637134351",
+"https://cdn.shopify.com/s/files/1/0449/5225/6667/files/Papaya-desktop-banner_1512x.jpg?v=1633939948"
 ];
 
-var info = ["MAKE THE BERRY SEASON MERRY","THE FRUIT OF THE ANGELS","ASSURED SAFETY","WE SERVE YOU THE"];
-var info1 = ["WITH KIMAYE BERRY COMBO", "KIMAYE PAPAYA", "FROM FARM TO HOME", "APPLES OF OUR EYE"];
-var info2 = ["Berry Season", "100% Traceable | Naturally Ripened", "We are #all safe all good", "Stringent quality check ensure only the best fruits earn the Kimaye badge"]
+var info = ["ASSURED SAFETY","WE SERVE YOU THE","MAKE THE BERRY SEASON MERRY","THE FRUIT OF THE ANGELS"];
+var info1 = ["FROM FARM TO HOME", "APPLES OF OUR EYE", "WITH KIMAYE BERRY COMBO","KIMAYE PAPAYA"];
+var info2 = ["We are #all safe all good", "Stringent quality check ensure only the best fruits earn the Kimaye badge", "Berry Season","100% Traceable | Naturally Ripened"]
 
 var slider_content = document.querySelector("#box");
 
 var head = document.querySelector("#box>div>h1");
 var head1 = document.querySelector("#box>div>h2");
 var head2 = document.querySelector("#box>div>p");
+
+var boxdiv = document.querySelector("#box>div");
 
 
 var i = image.length;
@@ -37,6 +39,8 @@ function nextimage(){
     head1.textContent = info1[i-1];
     head2.textContent = info2[i-1];
     head2.style.color = "black";
+    boxdiv.style.paddingTop = "100px";
+    boxdiv.style.transition= "0.75s all ease-in-out";
 }
 
 function previmage(){
@@ -60,12 +64,12 @@ function previmage(){
 document.querySelector(".next1").addEventListener("click", nextimage1);
 document.querySelector(".prev1").addEventListener("click", previmage1);
 
-var image1 = [ "https://images.pexels.com/photos/1750468/pexels-photo-1750468.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-"https://images.pexels.com/photos/4113868/pexels-photo-4113868.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+var image1 = [ "https://cdn.shopify.com/s/files/1/0449/5225/6667/articles/AdobeStock_77286816-min__1625567830_114.143.85.97_720x.jpg?v=1625638895",
+"https://cdn.shopify.com/s/files/1/0449/5225/6667/files/Untitled_design_51-min.png?v=1614342058",
 ];
 
-var image2 = [ "https://images.indianexpress.com/2021/09/fruits_getty.jpg",
-"https://images.pexels.com/photos/1098592/pexels-photo-1098592.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+var image2 = [ "https://cdn.shopify.com/s/files/1/0449/5225/6667/articles/Blog_-_Benefits_of_fruits_720x.jpg?v=1625147388",
+"https://cdn.shopify.com/s/files/1/0449/5225/6667/files/immune_system_1728x-min.jpg?v=1614342138",
 ];
 
 var infor1 = ["How To Remove And Clean Harmful Pesticides From Fruits","Grapes: How they reached India, types and health benefits"];
@@ -82,34 +86,34 @@ var heading3 = document.querySelector(".slider2>div:nth-child(2)>h2");
 var heading4 = document.querySelector(".slider2>div:nth-child(2)>p");
 
 
-var i = image1.length;
+var j = image1.length;
 
 function nextimage1(){
-    if(i<image.length){
-        i = i+1;
+    if(j<image1.length){
+        j = j+1;
     }
     else{
-        i = 1;
+        j = 1;
     }
-    slider_content1.style.backgroundImage = "url("+image1[i-1]+")";
-    slider_content2.style.backgroundImage = "url("+image2[i-1]+")";
-    heading1.textContent = infor1[i-1];
-    heading2.textContent = infor2[i-1];
-    heading3.textContent = infor3[i-1];
-    heading4.textContent = infor4[i-1];
+    slider_content1.style.backgroundImage = "url("+image1[j-1]+")";
+    slider_content2.style.backgroundImage = "url("+image2[j-1]+")";
+    heading1.textContent = infor1[j-1];
+    heading2.textContent = infor2[j-1];
+    heading3.textContent = infor3[j-1];
+    heading4.textContent = infor4[j-1];
 }
 
 function previmage1(){
-    if(i<image.length+1 && i>1){
-        i = i-1;
+    if(j<image1.length+1 && j>1){
+        j = j-1;
     }
     else{
-        i = image.length;
+        j = image1.length;
     }
-    slider_content1.style.backgroundImage = "url("+image1[i-1]+")";
-    slider_content2.style.backgroundImage = "url("+image2[i-1]+")";
-    heading1.textContent = infor1[i-1];
-    heading2.textContent = infor2[i-1];
-    heading3.textContent = infor3[i-1];
-    heading4.textContent = infor4[i-1];
+    slider_content1.style.backgroundImage = "url("+image1[j-1]+")";
+    slider_content2.style.backgroundImage = "url("+image2[j-1]+")";
+    heading1.textContent = infor1[j-1];
+    heading2.textContent = infor2[j-1];
+    heading3.textContent = infor3[j-1];
+    heading4.textContent = infor4[j-1];
 }
